28b7f68da0d284264a23542040051b864864cf74
Matthew Myers <matt2east@gmail.com>
2018-06-02 20:45:42 -0500

    conditional reward based on score

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,13 +11,19 @@ class Game extends Component {
       disabled2: false,
       disabled3: false,
       disabled4: false,
+      disabled5: false,
+      disabled6: false,
+      disabled999: false,
       isHidden: true
 
     };
 
-    this.handleWalkToWork = this.handleWalkToWork.bind(this);
-    this.handleIdleCar = this.handleIdleCar.bind(this);
-    this.handleSolarPanels = this.handleSolarPanels.bind(this);
+    this.handleSkipDriveThru = this.handleSkipDriveThru.bind(this);
+    this.handleCombineErrands = this.handleCombineErrands.bind(this);
+    this.handleElectricMower = this.handleElectricMower.bind(this);
+    this.handleBroom = this.handleBroom.bind(this);
+    this.handlePowerStrip = this.handlePowerStrip.bind(this);
+    this.handleComputerSleep = this.handleComputerSleep.bind(this);
     this.handleNuke = this.handleNuke.bind(this);
   }
   toggleHidden () {
@@ -26,28 +32,47 @@ class Game extends Component {
     })
   }
 
-  handleWalkToWork() {
+  handleSkipDriveThru() {
     this.setState({
       gameScore: this.state.gameScore +1,
       disabled1: !this.state.disabled1
     });
   } 
-  handleIdleCar() {
+  handleCombineErrands() {
     this.setState({
-      gameScore: this.state.gameScore -1,
+      gameScore: this.state.gameScore +1,
       disabled2: !this.state.disabled2
     });
   } 
-  handleSolarPanels() {
+  handleElectricMower() {
     this.setState({
-      gameScore: this.state.gameScore +10,
+      gameScore: this.state.gameScore +1,
       disabled3: !this.state.disabled3
     });
   }
+  handleBroom() {
+    this.setState({
+      gameScore: this.state.gameScore +1,
+      disabled4: !this.state.disabled4
+    });
+  }
+  handlePowerStrip() {
+    this.setState({
+      gameScore: this.state.gameScore +1,
+      disabled5: !this.state.disabled5
+    });
+  }
+  handleComputerSleep() {
+    this.setState({
+      gameScore: this.state.gameScore +1,
+      disabled6: !this.state.disabled6
+    });
+  }
+  
   handleNuke() {
     this.setState({
       gameScore: this.state.gameScore -999,
-      disabled4: !this.state.disabled4
+      disabled4: !this.state.disabled999
     });
   }
    
@@ -58,42 +83,69 @@ class Game extends Component {
 
         <h1>Play the ozone game.</h1>
       <form>
-   
-      Did you walk to work today?
+        Skipped the drive-thru and went inside instead.
           <input
             name="gameScore"
             type="checkbox"
             disabled = {(this.state.disabled1)}
-            onChange={this.handleWalkToWork} />
+            onChange={this.handleSkipDriveThru} />
             <br/>
-      Did you avoid idling your car today?
+      Combined errands into one trip.
           <input
             name="gameScore"
             type="checkbox"
             disabled = {(this.state.disabled2)? "disabled2" : ""}
-            onChange={this.handleIdleCar}
+            onChange={this.handleCombineErrands}
             />
             <br/>
-      Did you install solar panels?
+      Used my electric lawnmower.
           <input
             name="gameScore"
             type="checkbox"
             disabled = {(this.state.disabled3)? "disabled3" : ""}
-            onChange={this.handleSolarPanels}
+            onChange={this.handleElectricMower}
             />
             <br/>
-                Did you launch a nuke?
+            Used a broom to clean up yard waste instead of a leaf blower.
           <input
             name="gameScore"
             type="checkbox"
             disabled = {(this.state.disabled4)? "disabled4" : ""}
-            onChange={this.handleNuke}
+            onChange={this.handleBroom}
             />
+            <br/>
+            Plugged my appliances into a power strip to reduce vampire energy use.
+          <input
+            name="gameScore"
+            type="checkbox"
+            disabled = {(this.state.disabled5)? "disabled5" : ""}
+            onChange={this.handlePowerStrip}
+            />
+            <br/>
+            Set my computer to sleep.
+          <input
+            name="gameScore"
+            type="checkbox"
+            disabled = {(this.state.disabled6)? "disabled6" : ""}
+            onChange={this.handleComputerSleep}
+            />
+            <br/>
+                {/* Launched a nuke.
+          <input
+            name="gameScore"
+            type="checkbox"
+            disabled = {(this.state.disabled999)? "disabled999" : ""}
+            onChange={this.handleNuke}
+            /> */}
       </form>
       {this.state.isHidden && <button onClick={this.toggleHidden.bind(this)} >
           Get your score!
         </button>}
         {!this.state.isHidden && <p>Your score is <b>{this.state.gameScore}</b></p>}
+        {!this.state.isHidden && this.state.gameScore < 3 && <p>You suck!</p>}
+        {!this.state.isHidden && this.state.gameScore >= 3 &&  this.state.gameScore < 5 && <p>Average score. Meh.</p>}
+        {!this.state.isHidden && this.state.gameScore >= 5 && <p>Awesome!!!</p>}
+
       </div>
     );
   }
